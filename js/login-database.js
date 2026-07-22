31e54896fe20b3181c86e1b0e40608802d729c4f
deboragadis <139110594+deboragadis@users.noreply.github.com>
2026-07-23 01:13:58 +0700

    Update login-database.js

--- js/login-database.js
+++ js/login-database.js
@@ -2,9 +2,10 @@
 
 const registeredAccounts = [
   // Format: { username: "nama", password: "password", role: "hak_akses" }
-  { username: "admin", password: "adminkeren123", role: "admin" },
-  { username: "andi", password: "0000", role: "pm" },
-  { username: "ari", password: "0000", role: "pm" },
+  { username: "admin", password: "adminkeren123", role: "superadmin" },
+  { username: "andi", password: "0000", role: "manager" },
+  { username: "ari", password: "0000", role: "manager" },
+  { username: "taufan", password: "0000", role: "manager" },
   { username: "yanuar", password: "12345", role: "staff" },
   { username: "febbry", password: "12345", role: "staff" },
   { username: "debora", password: "12345", role: "staff" },
