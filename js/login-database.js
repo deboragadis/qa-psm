aa99d503ebdf6f487466f33b3cda6fc7484655e8
Debora Gadis <139110594+deboragadis@users.noreply.github.com>
2026-07-29 00:06:39 -0700

    Add files via upload

---
+++ js/login-database.js
@@ -0,0 +1,18 @@
+// File: database.js
+
+const registeredAccounts = [
+  // Format: { username: "nama", password: "password", role: "hak_akses" }
+  { username: "admin", password: "0000", role: "superadmin" },
+  { username: "gadis", password: "adminkeren123", role: "superadmin" },
+  { username: "insanu", password: "adminkeren123", role: "superadmin" },
+  { username: "andi", password: "0000", role: "manager" },
+  { username: "ari", password: "0000", role: "manager" },
+  { username: "taufan", password: "0000", role: "manager" },
+  { username: "yanuar", password: "12345", role: "staff" },
+  { username: "febbry", password: "12345", role: "staff" },
+  { username: "debora", password: "12345", role: "staff" },
+  { username: "bima", password: "12345", role: "staff" },
+  
+];
+
+localStorage.setItem("active_user_role", akunDitemukan.role);
